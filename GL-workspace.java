590974a449320d60a88c20134ddb53aebd9e92bd
renuka1983 <65768563+renuka1983@users.noreply.github.com>
2022-05-07 18:35:11 +0530

    Update GL-workspace.java

--- GL-workspace.java
+++ GL-workspace.java
@@ -1,7 +1,7 @@
 public class GL-workspace {
-    public static void main(String.args[]){
-        int index = 0
-        system.out.println(index)
+    public static void main(String[] args){
+        int index = 0;
+        system.out.println(index);
     }
     
 }
