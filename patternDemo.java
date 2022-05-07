e1cb5d41db5a83792f19fed52735721709d92b39
renuka1983 <65768563+renuka1983@users.noreply.github.com>
2022-05-07 18:36:39 +0530

    nested while loop

---
+++ patternDemo.java
@@ -0,0 +1,17 @@
+public class patternDemo {
+    public static void main(String[] args) {
+        int row = 1;
+        while(row<=3)
+        {
+            int col=1;
+            while(col <=3)
+            {
+                System.out.println(col+" ");
+                col ++;
+            }
+            System.out.println();
+            row ++;
+        }
+    }
+    
+}
